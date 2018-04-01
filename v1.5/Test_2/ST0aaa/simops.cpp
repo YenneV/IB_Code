755cb8bffd13beeccc29e3b1f71d32a91b258dad
Helen Marley <xhelenmariex@yahoo.com>
2018-04-01 21:36:47 +0100

    Test 2 revised and tested at closer than equilibrium

--- v1.5/Test_2/ST0aaa/simops.cpp
+++
@@ -1,7 +0,0 @@
-#define CONC false         //don't include concentration on the boundary
-#define BULKPOL false         //don't include internal active fluid
-#define WALLS false         //don't include walls
-const int Lx = 120;         //number of point in x direction
-const int Ly = 120;          //number of points in y direction
- 
-double ST=0;    //bare surface tension
